99a230234a57268f95e7e69ef7668532f91d615e
rafietrana <bdmail674@gmail.com>
2024-04-29 15:08:57 +0600

    add this code

--- index.js
+++ index.js
@@ -29,106 +29,92 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    const artCollection = client.db('artDB').collection('art');
+    const artCollection = client.db("artDB").collection("art");
+    const subCollection = client.db("artDB").collection("subcategory");
 
+    app.post("/postdata", async (req, res) => {
+      const artData = req.body;
+      console.log(" art data is", artData);
+      const result = await artCollection.insertOne(artData);
+      res.send(result);
+    });
 
-     app.post('/postdata', async(req, res)=>{
-                const artData = req.body;
-                console.log(' art data is', artData);
-                const result = await artCollection.insertOne(artData);
-                res.send(result)
+    app.get("/postdata", async (req, res) => {
+      const cursor = artCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-     });
+    app.get("/mycard/:email", async (req, res) => {
+      const emails = req.params.email;
+      const query = { User_Email: emails };
+      const result = await artCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/finddata/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artCollection.find(query).toArray();
+      res.send(result);
+    });
 
+    app.get("/finddata/:subcategory", async (req, res) => {
+      const subcategory = req.params.subcategory;
+      const filter = { Subcategory_Name: subcategory };
+      const result = await subCollection.find(filter).toArray();
+      res.send(result);
+    });
 
-
-
-
-     app.get('/postdata', async(req, res)=>{
-            const cursor =   artCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
- 
     
-     });
 
 
-   app.get( '/mycard/:email', async(req,res)=>{
-       const emails =  req.params.email;
-       const query = {User_Email : emails}
-       const result = await artCollection.find(query).toArray()
-       res.send(result)
-        
- 
+    app.get('/subdata', async(req, res)=>{
+      const cursor = subCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
 
-
-   })
-   app.get( '/finddata/:id', async(req,res)=>{
-       const id =  req.params.id;
-       const query = {_id: new ObjectId(id)}
-       const result = await artCollection.find(query).toArray()
-       res.send(result)
-        
-
-   })
-
-
-
-   app.put('/updatedata/:id', async(req, res)=>{
-    const ids = req.params.id;
-    console.log('ids is ', ids);
+    app.put("/updatedata/:id", async (req, res) => {
+      const ids = req.params.id;
+      console.log("ids is ", ids);
       const updateInfo = req.body;
-      console.log('updateInfo is', updateInfo);
+      console.log("updateInfo is", updateInfo);
 
-
-      const filter = {_id: new ObjectId(ids)}
+      const filter = { _id: new ObjectId(ids) };
 
       const options = { upsert: true };
 
-      const updateDoc ={
-        $set:{
-          
- 
-        Item_name:updateInfo.Item_name,
-        Image:updateInfo.Image,
-        Subcategory_Name:updateInfo.Subcategory_Name,
-        Short_Description:updateInfo.Short_Description,
-        Price:updateInfo.Price,
-        Procassing_Time:updateInfo.Procassing_Time,
-        Customization:updateInfo.Customization,
-        Rating:updateInfo.Rating,
-        Stuck_status:updateInfo.Stuck_status
-        }
+      const updateDoc = {
+        $set: {
+          Item_name: updateInfo.Item_name,
+          Image: updateInfo.Image,
+          Subcategory_Name: updateInfo.Subcategory_Name,
+          Short_Description: updateInfo.Short_Description,
+          Price: updateInfo.Price,
+          Procassing_Time: updateInfo.Procassing_Time,
+          Customization: updateInfo.Customization,
+          Rating: updateInfo.Rating,
+          Stuck_status: updateInfo.Stuck_status,
+        },
+      };
+      const result = await artCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
- 
+    app.delete("/deletedata/:id", async (req, res) => {
+      const id = req.params.id;
 
+      const filter = { _id: new ObjectId(id) };
+      const result = await artCollection.deleteOne(filter);
+      res.send(result);
+    });
 
-      }
-      const result  = await artCollection.updateOne(filter, updateDoc, options)
-      res.send(result)
-   })
-
-
-   app.delete('/deletedata/:id', async(req, res)=>{
-    const id = req.params.id;
- 
-    const filter = {_id: new ObjectId(id)};
-    const result = await artCollection.deleteOne(filter);
-    res.send(result)
-   })
-
-
- 
-
-
-
- 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
     // await client.close();
   }
 }
