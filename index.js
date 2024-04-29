e93ca308949597be2892116ed090e4e1a1bf8035
rafietrana <bdmail674@gmail.com>
2024-04-29 09:37:22 +0600

    add this code

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("my server is now running ");
+  res.send("my server is now running sucessfully");
 });
 
 app.listen(port, () => {
